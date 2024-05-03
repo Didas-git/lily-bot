575088ef880a3784363c244098501de7904bff0a
DidaS <didasoficial@gmail.com>
2024-05-03 16:33:08 +0100

    Add mdn search command

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
-import { createClient, Intents, InteractionCallbackType, InteractionType } from "lilybird";
+import { ApplicationCommandOptionType, createClient, Intents, InteractionCallbackType, InteractionType } from "lilybird";
 import { handleGithubURLInMessage } from "./github.js";
-import { Handler } from "./handler.js";
+import { CommandManager } from "./handler.js";
+import { handleMDNAutocomplete, handleMDNInteraction } from "./mdn.js";
 
-const handler = new Handler();
+const handler = new CommandManager();
 
 handler.addCommand({
     name: "ping",
     description: "pong"
 });
 
-export function safeSlice<T extends string | Array<any>>(
-    input: T,
-    length: number
-): T {
-    return <T>(input.length > length ? input.slice(0, length) : input);
-}
+handler.addCommand({
+    name: "mdn",
+    description: "Search the MDN documentation",
+    options: [
+        {
+            type: ApplicationCommandOptionType.STRING,
+            name: "query",
+            description: "Search query to pass to mdn",
+            required: true,
+            autocomplete: true
+        },
+        {
+            type: ApplicationCommandOptionType.USER,
+            name: "user",
+            description: "Ping a user along with the response"
+        }
+    ]
+});
+
 await createClient({
     token: process.env.TOKEN,
     intents: [
@@ -32,7 +46,15 @@ await createClient({
             await handleGithubURLInMessage(client, payload);
         },
         interactionCreate: async (client, payload) => {
+            if (!("guild_id" in payload)) return;
+
+            if (payload.type === InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE) {
+                await handleMDNAutocomplete(client, payload);
+                return;
+            }
+
             if (payload.type !== InteractionType.APPLICATION_COMMAND) return;
+
             switch (payload.data.name) {
                 case "ping": {
                     const { ws, rest } = await client.ping();
@@ -40,6 +62,9 @@ await createClient({
                         type: InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
                         data: { content: `🏓 WebSocket: \`${Math.round(ws)}ms\` | Rest: \`${Math.round(rest)}ms\`` }
                     });
+                }
+                case "mdn": {
+                    return handleMDNInteraction(client, payload);
                 }
             }
 
---
+++ src/mdn.ts
@@ -0,0 +1,121 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { InteractionCallbackType, MessageFlags, type Client, type Interaction } from "lilybird";
+
+interface GoogleAPIResponse {
+    kind: string;
+    url: Record<string, unknown>;
+    queries: Record<string, unknown>;
+    context: {
+        title: string
+    };
+    searchInformation: Record<string, unknown>;
+    items: Array<GoogleAPIItem>;
+}
+
+interface GoogleAPIItem {
+    kind: string;
+    title: string;
+    htmlTitle: string;
+    link: string;
+    displayLink: string;
+    snippet: string;
+    htmlSnippet: string;
+    cacheId: string;
+    formattedUrl: string;
+    htmlFormattedUrl: string;
+    pagemap: {
+        cse_thumbnail: Array<{ src: string, width: string, height: string }>,
+        xfn: Array<Record<string, unknown>>,
+        BreadcrumbList: Array<Record<string, unknown>>,
+        metatags: Array<Metatag>,
+        cse_image: Array<{ src: string }>
+    };
+}
+
+interface Metatag {
+    "og:image": string;
+    "theme-color": string;
+    "og:type": string;
+    "og:image:width": string;
+    "og:image:alt": string;
+    "twitter:card": string;
+    "og:site_name": string;
+    "og:title": string;
+    "og:image:height": string;
+    "og:image:type": string;
+    "og:description": string;
+    "twitter:creator": string;
+    viewport: string;
+    "og:locale": string;
+    position: string;
+    "og:url": string;
+}
+
+const localMDNCache = new Map<string, Metatag>();
+
+function populateCache(items: Array<GoogleAPIItem>): void {
+    for (let i = 0, { length } = items; i < length; i++) {
+        const item = items[i];
+        const meta = item.pagemap.metatags;
+
+        if (localMDNCache.has(item.cacheId)) continue;
+
+        localMDNCache.set(item.cacheId, meta[0]);
+    }
+}
+
+export async function handleMDNAutocomplete(client: Client, interaction: Interaction.GuildApplicationCommandInteractionStructure): Promise<void> {
+    if (typeof interaction.data.options === "undefined") return;
+
+    let i = 0;
+    while (!interaction.data.options[i].focused) i++;
+
+    const query = interaction.data.options[i].value;
+    if (typeof query !== "string") return;
+    if (query.length === 0) return;
+
+    const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.SEARCH_KEY}&cx=${process.env.CX}&q=${query}&num=10`;
+
+    const response = await fetch(url);
+    if (!response.ok) return;
+    const body: GoogleAPIResponse = await response.json() as never;
+
+    populateCache(body.items);
+
+    await client.rest.createInteractionResponse(interaction.id, interaction.token, {
+        type: InteractionCallbackType.APPLICATION_COMMAND_AUTOCOMPLETE_RESULT,
+        data: { choices: body.items.map((val) => ({ name: val.title, value: val.cacheId })) }
+    });
+}
+
+export async function handleMDNInteraction(client: Client, interaction: Interaction.GuildApplicationCommandInteractionStructure): Promise<void> {
+    const cacheId = interaction.data.options?.find((op) => op.name === "query")?.value;
+    if (typeof cacheId === "undefined" || typeof cacheId !== "string") {
+        await client.rest.createInteractionResponse(interaction.id, interaction.token, {
+            type: InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: "Something went wrong", flags: MessageFlags.EPHEMERAL }
+        });
+        return;
+    }
+    const tags = localMDNCache.get(cacheId);
+    if (!tags) throw new Error("Internal MDN cache logic error");
+
+    const userId = interaction.data.options?.find((op) => op.name === "user")?.value;
+
+    await client.rest.createInteractionResponse(interaction.id, interaction.token, {
+        type: InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+            content: typeof userId === "string" ? `<@${userId}> learn how to fucking google` : "",
+            embeds: [
+                {
+                    title: tags["og:title"],
+                    description: tags["og:description"],
+                    url: tags["og:url"],
+                    image: {
+                        url: tags["og:image"]
+                    }
+                }
+            ]
+        }
+    });
+}
